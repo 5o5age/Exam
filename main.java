1d186341513ce2d23e8ea5152193cb1414a7f23e
Nils Terentjevs <A230760NT@rvt.lv>
2025-03-20 12:46:00 +0200

    Added males.java and names.csv for the file reading testing purposes

--- main.java
+++ main.java
@@ -1,8 +1,18 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) throws Exception {
         
+            File file = new File("src\\names.csv");
+            Set<Male> males = new TreeSet<>();
+
+
+            try {
+                Scanner inputStream = new Scanner(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         
     }
 }
